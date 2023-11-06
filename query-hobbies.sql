5291665aec0683357744e47aac7ac9d9658f543c
Cahya <cahyakumala573@gmail.com>
2023-11-06 10:52:31 +0800

    query-hobbies(5)

--- query-hobbies.sql
+++ query-hobbies.sql
@@ -5,7 +5,8 @@ SELECT p.id   AS ID,
        h.name AS Hobi
 FROM persons p
          INNER JOIN person_hobbies ph ON p.id = ph.person_id
-         INNER JOIN hobbies h ON ph.hobby_id = h.id;
+#          INNER JOIN hobbies h ON ph.hobby_id = h.id;
+         INNER JOIN hobbies h ON h.id = ph.hobby_id;
 
 # hobbies 2
 SELECT p.id   AS ID,
@@ -36,10 +37,33 @@ FROM persons p
 UNION
 SELECT p.id   AS ID,
        p.name AS Name,
-       h.id   AS ID,
+       h.id   AS "Hobi ID",
        h.name AS Hobi
 FROM persons p
          RIGHT JOIN person_hobbies ph ON p.id = ph.person_id
          RIGHT JOIN hobbies h ON ph.hobby_id = h.id
 WHERE p.id IS NULL;
 
+# hobbies 5
+SELECT IFNULL(p.id, "-")                 AS "Person ID",
+       IFNULL(p.name, "-")               AS Name,
+       IFNULL(GROUP_CONCAT(h.name), "-") AS Hobi
+
+FROM persons p
+         LEFT JOIN person_hobbies ph ON p.id = ph.person_id
+         LEFT JOIN hobbies h ON ph.hobby_id = h.id
+GROUP BY p.id
+
+UNION
+SELECT IFNULL(p.id, "-")                 AS "Person ID",
+       IFNULL(p.name, "-")               AS Name,
+       IFNULL(GROUP_CONCAT(h.name), "-") AS Hobi
+FROM persons p
+         RIGHT JOIN person_hobbies ph ON p.id = ph.person_id
+         RIGHT JOIN hobbies h ON ph.hobby_id = h.id
+
+WHERE p.id IS NULL
+GROUP BY p.id
+
+ORDER BY `Person ID` ASC
+;
